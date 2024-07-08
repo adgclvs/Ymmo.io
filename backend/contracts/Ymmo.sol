182101d057436945a17c4afebc99d4697f403675
adgclvs <adriengoncalves2003@gmail.com>
2024-07-08 11:00:39 +0200

    add NatSpecs

--- backend/contracts/Ymmo.sol
+++ backend/contracts/Ymmo.sol
@@ -7,15 +7,37 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "./Token.sol";
 
+/**
+ * @title Ymmo Contract
+ * @notice This contract allows for the creation, purchase, and income distribution of Ymmo tokens.
+ * @dev Inherits from Ownable and ReentrancyGuardUpgradeable.
+ */
 contract Ymmo is Ownable, ReentrancyGuardUpgradeable {
     uint128 public valueOfYmmo;
     uint64 public indexOfYmmo;
     uint64 public valueIncome;
     IERC20 public tokenContract;
 
+    /**
+     * @notice Event emitted when tokens are purchased.
+     * @param buyer The address of the buyer.
+     * @param amount The amount of tokens purchased.
+     * @param ethSpent The amount of ETH spent.
+     */
     event TokensPurchased(address buyer, uint256 amount, uint256 ethSpent);
+
+    /**
+     * @notice Event emitted when income is distributed.
+     * @param _address The address receiving the income.
+     * @param _amount The amount of income distributed.
+     */
     event IncomeDistributed(address _address, uint256 _amount);
 
+    /**
+     * @notice Constructor that initializes the contract with a value and index for Ymmo.
+     * @param _valueOfYmmo The initial value of the Ymmo contract.
+     * @param _indexOfYmmo The index of the Ymmo contract.
+     */
     constructor(uint128 _valueOfYmmo, uint64 _indexOfYmmo) Ownable(msg.sender) {
         require(_valueOfYmmo > 0, "Value of Ymmo must be greater than 0");
         valueOfYmmo = _valueOfYmmo;
@@ -25,6 +47,9 @@ contract Ymmo is Ownable, ReentrancyGuardUpgradeable {
         tokenContract = IERC20(new Token(_valueOfYmmo, name, symbol));
     }
 
+    /**
+     * @notice Allows users to buy Ymmo tokens using ETH.
+     */
     function buyTokens() external payable {
         require(msg.value > 0, "You need to send some ETH");
 
@@ -41,11 +66,18 @@ contract Ymmo is Ownable, ReentrancyGuardUpgradeable {
         emit TokensPurchased(msg.sender, amountToBuy, msg.value);
     }
 
+    /**
+     * @notice Sets the value of income that can be distributed.
+     * @param _valueIncome The value of the income to set.
+     */
     function setValueIncome(uint64 _valueIncome) external onlyOwner {
         require(_valueIncome <= valueOfYmmo, "the income cannot be greater than the value of Ymmo");
         valueIncome = _valueIncome;
     }
 
+    /**
+     * @notice Allows users to claim their share of the income.
+     */
     function getIncome() external payable nonReentrant {
         uint256 tokenBalance = tokenContract.balanceOf(msg.sender);
         require(tokenBalance > 0, "No YMmo tokens owned");
@@ -60,11 +92,19 @@ contract Ymmo is Ownable, ReentrancyGuardUpgradeable {
         emit IncomeDistributed(msg.sender, income);
     }
 
+    /**
+     * @notice Allows the owner to withdraw ETH from the contract.
+     * @param _to The address to send the ETH to.
+     * @param amount The amount of ETH to withdraw.
+     */
     function withdrawETH(address _to, uint256 amount) external payable onlyOwner nonReentrant {
         require(address(this).balance >= amount, "Insufficient balance");
         (bool success, ) = _to.call{value: amount}("");
         require(success, "Withdraw failed");
     }
 
+    /**
+     * @notice Fallback function to receive ETH.
+     */
     receive() external payable {}
 }
--- backend/contracts/YmmoFactory.sol
+++ backend/contracts/YmmoFactory.sol
@@ -4,19 +4,36 @@ pragma solidity 0.8.24;
 import "./Ymmo.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title YmmoFactory Contract
+ * @notice This contract is used to create and manage Ymmo contracts.
+ * @dev Inherits from Ownable to manage ownership of the contract.
+ */
 contract YmmoFactory is Ownable {
     Ymmo[] private listOfYmmo;
 
+    /**
+     * @notice Event emitted when a new Ymmo contract is deployed.
+     * @param contractAddress The address of the newly deployed Ymmo contract.
+     */
     event NewContractYmmoDeploy(address contractAddress);
 
     constructor() Ownable(msg.sender) {}
 
+    /**
+     * @notice Creates a new Ymmo contract.
+     * @param _valueOfYmmo The initial value of the Ymmo contract.
+     */
     function createYmmo(uint128 _valueOfYmmo) external onlyOwner {
         Ymmo ymmo = new Ymmo(_valueOfYmmo, uint64(listOfYmmo.length + 1));
         listOfYmmo.push(ymmo);
         emit NewContractYmmoDeploy(address(ymmo));
     }
 
+    /**
+     * @notice Returns the list of deployed Ymmo contracts.
+     * @return An array of Ymmo contract addresses.
+     */
     function getListOfYmmo() external view returns (Ymmo[] memory) {
         return listOfYmmo;
     }
