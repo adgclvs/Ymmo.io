a74a40da048a231320cdbaed4057e0c857878fde
adgclvs <adriengoncalves2003@gmail.com>
2024-07-14 15:04:54 +0200

    .

--- frontend/components/shared/OneYmmo.jsx
+++ frontend/components/shared/OneYmmo.jsx
@@ -1,6 +1,5 @@
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { parseEther } from "viem";
 import { useAccount, useBalance, useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
